d8f1db240d5bce677763b87b63b069bd85ee8da3
Stanley Odhiambomwai <odhiambostanley799@gmail.com>
2025-03-25 09:56:19 +0300

    adding the home page

---
+++ frontend/src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="text-center p-10">
+      <h2 className="text-3xl font-bold">Welcome to Our Store</h2>
+      <p className="mt-4">Find the best products here.</p>
+    </div>
+  );
+};
+
+export default Home;
