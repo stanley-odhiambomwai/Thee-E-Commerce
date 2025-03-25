eab87d162e746c04881dc1c1f38e0e26cd8b1893
Stanley Odhiambomwai <odhiambostanley799@gmail.com>
2025-03-25 10:04:54 +0300

    adding the Cart page

---
+++ frontend/src/Pages/Cart.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Cart = () => {
+  return (
+    <div className="p-10">
+      <h2 className="text-3xl font-bold">Your Cart</h2>
+      <p>Your selected items will appear here.</p>
+    </div>
+  );
+};
+
+export default Cart;
