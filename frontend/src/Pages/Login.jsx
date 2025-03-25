fb4b3165035445a410098541875893c32abf0cfe
Stanley Odhiambomwai <odhiambostanley799@gmail.com>
2025-03-25 10:08:18 +0300

    adding the Login page

---
+++ frontend/src/Pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div className="p-10">
+      <h2 className="text-3xl font-bold">Login</h2>
+      <p>Sign in to access your account.</p>
+    </div>
+  );
+};
+
+export default Login;
