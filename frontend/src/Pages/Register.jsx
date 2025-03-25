b47d9183d9becb3c98bec6caf1d3083c0c9d096c
Stanley Odhiambomwai <odhiambostanley799@gmail.com>
2025-03-25 10:10:16 +0300

    adding the Register page

---
+++ frontend/src/Pages/Register.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Register = () => {
+  return (
+    <div className="p-10">
+      <h2 className="text-3xl font-bold">Register</h2>
+      <p>Create an account to start shopping.</p>
+    </div>
+  );
+};
+
+export default Register;
