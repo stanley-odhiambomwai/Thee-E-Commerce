23e660c99d2e38303eb69d575e30ec0558d691c0
Stanley Odhiambomwai <odhiambostanley799@gmail.com>
2025-03-25 09:53:54 +0300

    adding the footer component

---
+++ frontend/src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 p-4 text-white text-center">
+      <p>&copy; 2025 E-Commerce. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
