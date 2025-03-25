891d2a1e8d3d2e1cd46269e9ab76bb24b2385400
Stanley Odhiambomwai <odhiambostanley799@gmail.com>
2025-03-25 10:06:26 +0300

    adding the Checkout page

---
+++ frontend/src/Pages/Checkout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Checkout = () => {
+  return (
+    <div className="p-10">
+      <h2 className="text-3xl font-bold">Checkout</h2>
+      <p>Complete your order.</p>
+    </div>
+  );
+};
+
+export default Checkout;
